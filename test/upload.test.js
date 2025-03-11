a1b2dfdc36771febd3da467a6a6dc1967af58166
Varshika Dhevanayagam <varshika.20210977@iit.ac.lk>
2025-03-11 10:14:37 +0530

    undo previous commit

--- test/upload.test.js
+++ test/upload.test.js
@@ -9,14 +9,12 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const serviceAccount = JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT_JSON);
-
 const SCOPES = [
     'https://www.googleapis.com/auth/drive.file',
     'https://www.googleapis.com/auth/spreadsheets'
 ];
 const auth = new google.auth.GoogleAuth({
-    keyFile: serviceAccount,
+    keyFile: './cv-pipeline-01-92372bcf22b4.json',
     scopes: SCOPES,
 });
 const sheets = google.sheets({ version: 'v4', auth });
